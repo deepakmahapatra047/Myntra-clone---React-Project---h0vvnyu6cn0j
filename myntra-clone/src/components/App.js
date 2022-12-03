77e96e5c64c3d8531f3441e6657e729d912bbd07
Deepak Mahapatra <deepakmahapatra047@gmail.com>
2022-12-03 15:13:26 +0530

    y

---
+++ myntra-clone/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/App.css';
+import data from '../../data';
+import Header from './Header/Header';
+
+const App = () => {
+
+  return (
+    <div id="main">
+      
+      <Header/>
+    </div>
+  )
+}
+
+
+export default App;
